093125cd0120fed01db5444a1d6c12229d9df45f
KarenMartinez <k.nathalia.martinez.b@gmail.com>
2026-06-13 08:22:04 -0500

    enlaces y giro

---
+++ app.js
@@ -0,0 +1,11 @@
+function voltear(tarjeta){
+    tarjeta.classList.toggle("girada");
+}
+
+document.getElementById("btn-linkedin").addEventListener("click", function() {
+    window.open("https://www.linkedin.com/in/karen-nathalia-martinez-biscunda-full-stack", "_blank");
+});
+
+document.getElementById("btn-github").addEventListener("click", function() {
+    window.open("https://github.com/Nathalia010", "_blank");
+});
